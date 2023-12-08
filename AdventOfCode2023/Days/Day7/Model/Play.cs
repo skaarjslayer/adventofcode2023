a5121b82f5dd4ae6a178c6b0a29de6ebeac879d8
Cameron Gazey <cameron.gazey@snowedin.ca>
2023-12-07 19:33:40 -0500

    Partial solution for Day 7 Part 1 - switching computers

---
+++ AdventOfCode2023/Days/Day7/Model/Play.cs
@@ -0,0 +1,28 @@
+﻿namespace Day7.Model
+{
+    public class Play
+    {
+        public IEnumerable<Card> Hand { get; init; }
+        public int Bid { get; init; }
+
+        public Play(IEnumerable<Card> hand, int bid)
+        {
+            Hand = hand;
+            Bid = bid;
+        }
+
+        public int GetStrength()
+        {
+            HashSet<char> uniqueCharacters = new HashSet<char>(HandValue);
+
+            switch (uniqueCharacters.Count)
+            {
+                case 1: return 7;
+                case 2: return uniqueCharacters.Any(x => HandValue.Count(y => x == y) == 4) ? 6 : 5;
+                case 3: return uniqueCharacters.Any(x => HandValue.Count(y => x == y) == 3) ? 4 : 3;
+                case 4: return 2;
+                default: return 1;
+            }
+        }
+    }
+}
---
+++ AdventOfCode2023/Days/Day7/Services/Day7SolverService.cs
@@ -0,0 +1,33 @@
+﻿using Day7.Model;
+using Services;
+
+namespace Day7.Services
+{
+    public class Day7SolverService : ISolverService
+    {
+        private readonly IParseService<string, IEnumerable<Play>> _handParseService;
+
+        public Day7SolverService(IParseService<string, IEnumerable<Play>> handParseService)
+        {
+            _handParseService = handParseService;
+        }
+
+        public void Execute()
+        {
+            ExecuteS1(Day7.Resources.Resource.Test);
+            ExecuteS1(Day7.Resources.Resource.D7);
+            ExecuteS2(Day7.Resources.Resource.Test);
+            ExecuteS2(Day7.Resources.Resource.D7);
+        }
+
+        public void ExecuteS1(string data)
+        {
+            IEnumerable<Play> hands = _handParseService.Parse(data);
+
+        }
+
+        public void ExecuteS2(string data)
+        {
+        }
+    }
+}
