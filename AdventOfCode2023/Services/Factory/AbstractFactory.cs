a7c589f3d7290e57c20680ff50372dcd137ea777
Cameron <camerongazey@live.ca>
2023-12-10 14:09:29 -0500

    Comments for abstract factory

--- AdventOfCode2023/Services/Factory/AbstractFactory.cs
+++ AdventOfCode2023/Services/Factory/AbstractFactory.cs
@@ -2,18 +2,48 @@
 
 namespace Services
 {
+    /// <summary>
+    /// An abstract base class for all factories. Takes in data from an input type and uses that to produce an output.
+    /// </summary>
+    /// <typeparam name="TInput">The input type being used for the creation process.</typeparam>
+    /// <typeparam name="TOutput">The output type being used for the creation process.</typeparam>
     public abstract class AbstractFactory<TInput, TOutput>
     {
+        #region Public Methods
+
+        #region Abstract Methods
+
+        /// <summary>
+        /// Creates a single TOutput object from a single TInput object.
+        /// </summary>
+        /// <param name="input">The input object of type TInput.</param>
+        /// <returns>A new instance of TOutput.</returns>
+        /// <remarks>Use this method when you want to create a single object.</remarks>
         public abstract TOutput Create(TInput input);
 
+        #endregion Abstract Methods
+
+        /// <summary>
+        /// Creates multiple TOutput objects from multiple TInput objects.
+        /// </summary>
+        /// <param name="inputs">An IEnumerable of TInput objects.</param>
+        /// <returns>An IEnumerable of TOutput objects.</returns>
+        /// <remarks>Use this method when you want to create multiple objects that are different from one another.</remarks>
         public IEnumerable<TOutput> CreateMany(IEnumerable<TInput> inputs)
         {
-            foreach(TInput input in inputs)
+            foreach (TInput input in inputs)
             {
                 yield return Create(input);
             }
         }
 
+        /// <summary>
+        /// Creates multiple TOutput objects from a single TInput object.
+        /// </summary>
+        /// <param name="input">The input object of type TInput.</param>
+        /// <param name="count">The number of times to repeat the creation process.</param>
+        /// <returns>An IEnumerable of TOutput objects.</returns>
+        /// <remarks>Use this method when you want to create multiple objects that are the same.</remarks>
         public IEnumerable<TOutput> CreateMany(TInput input, int count)
         {
             for (int i = 0; i < count; i++)
@@ -21,5 +51,7 @@ namespace Services
                 yield return Create(input);
             }
         }
+
+        #endregion Public Methods
     }
 }
