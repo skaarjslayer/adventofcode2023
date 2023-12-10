0af21bf6d19479748d4860c48c9418c0ca90685b
Cameron <camerongazey@live.ca>
2023-12-10 14:09:16 -0500

    Comments for solver service

--- AdventOfCode2023/Services/SolverService/ISolverService.cs
+++ AdventOfCode2023/Services/SolverService/ISolverService.cs
@@ -1,7 +1,13 @@
 ﻿namespace Services
 {
+    /// <summary>
+    /// Handles logic for solving a variety of Advent of Code puzzles.
+    /// </summary>
     public interface ISolverService
     {
+        /// <summary>
+        /// Executes puzzle-solving strategies and outputs the result to the console window.
+        /// </summary>
         void Execute();
     }
 }
