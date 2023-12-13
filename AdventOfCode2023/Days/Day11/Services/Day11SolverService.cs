1c8525779d2b97bc09e0520683cbef13f6c3f5e5
Cameron <camerongazey@live.ca>
2023-12-13 18:14:22 -0500

    Day 11 Star 1 solution

--- AdventOfCode2023/Days/Day11/Services/Day11SolverService.cs
+++ AdventOfCode2023/Days/Day11/Services/Day11SolverService.cs
@@ -24,6 +24,20 @@ namespace Day11.Services
         public void ExecuteS1(string data)
         {
             Grid<SpaceCell> grid = _spaceGridFactory.Create(data.Split("\r\n"));
+            IEnumerable<SpaceCell> galaxies = grid.Cells.SelectMany(x => x).Where(x => x.Character == '#');
+            List<int> distance = new List<int>();
+
+            foreach (SpaceCell galaxy in galaxies)
+            {
+                foreach(SpaceCell otherGalaxy in galaxies.Where(x => x != galaxy))
+                {
+                    distance.Add(Math.Abs(galaxy.X - otherGalaxy.X) + Math.Abs(galaxy.Y - otherGalaxy.Y));
+                }
+            }
+
+            Console.WriteLine($"The sum of all galaxy-pair distance is {distance.Sum()/2}");
+
+            Console.ReadKey();
         }
 
         public void ExecuteS2(string data)
